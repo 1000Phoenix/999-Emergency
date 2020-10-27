7afe4373c2092ab803a90dd2a2301b2d2854db32
mattdavidj <69766912+mattdavidj@users.noreply.github.com>
2020-10-27 21:30:15 +0000

    Add files via upload

---
+++ lua/autorun/matt_sprinter_lwb.lua
@@ -0,0 +1,12 @@
+local V = {
+			Name = "999Pack Mercedes Sprinter LWB", 
+			Class = "prop_vehicle_jeep",
+			Category = "LW Cars",
+			Author = "LoneWolfie, MattDavidJ",
+			Information = "Driveable merc_sprinter_lwb by LoneWolfie",
+			Model = "models/LoneWolfie/matt_sprinter_lwb.mdl",	
+			KeyValues = {
+							vehiclescript	=	"scripts/vehicles/LWCars/matt_sprinter.txt"
+							}
+			}
+list.Set("Vehicles", "matt_sprinter_lwb_lw", V)
