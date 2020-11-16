88647f0eee435477b8a6e0dec02485c1d5f95c24
creator_2013 <creator.email.yt@gmail.com>
2020-11-16 16:04:49 +0100

    Make function local

--- lua/autorun/photon/library/auto/creator_woodway_perimeter_scene_light.lua
+++ lua/autorun/photon/library/auto/creator_woodway_perimeter_scene_light.lua
@@ -41,7 +41,7 @@ COMPONENT.Modes = {
 	}
 }
 
-function registerComponent( name, mode )
+local function registerComponent( name, mode )
 	local COMP = table.Copy( COMPONENT )
 	COMP.Modes.Illumination[ mode ] = { { 1, W } }
 	EMVU:AddAutoComponent( COMP, "Woodway Perimeter Scene Light " .. name )
