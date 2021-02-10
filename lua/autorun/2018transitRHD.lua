a82a51d369756787e762960e8aacda0681cc0076
mattdavidj <69766912+mattdavidj@users.noreply.github.com>
2021-02-10 20:02:37 +0000

    Add files via upload

---
+++ lua/autorun/2018transitRHD.lua
@@ -0,0 +1,19 @@
+local V = {
+	-- Required information
+	Name = "2018 Ford Transit RHD",
+	Model = "models/mdj/ford/2018_transitRHD.mdl",
+	Class = "prop_vehicle_jeep",
+	Category = "MattDavidJ's Vehicles",
+
+	-- Optional information
+	Author = "MattDavidJ",
+	Information = "uwu",
+
+	KeyValues = {
+
+		vehiclescript = "scripts/vehicles/mdj/2018transit.txt"
+	}
+}
+list.Set( "Vehicles", "2018transitRHD", V )
+
+
---
+++ lua/autorun/sfiretrucknew.lua
@@ -0,0 +1,17 @@
+local V = {
+	-- Required information
+	Name = "Scania Firetruck 2.0",
+	Model = "models/mdj/scania/scania fire truck new.mdl",
+	Class = "prop_vehicle_jeep",
+	Category = "MattDavidJ's Vehicles",
+
+	-- Optional information
+	Author = "MattDavidJ",
+	Information = "uwu",
+
+	KeyValues = {
+
+		vehiclescript = "scripts/vehicles/mdj/sfiretrucknew.txt"
+	}
+}
+list.Set( "Vehicles", "sfiretrucknew", V )
